9c26daba4374a052ae5b5517fd5f779fb0efa5f0
Shiela Mae Bacalando <hermetaniosheena@gmail.com>
2026-05-11 20:17:48 +0800

    fixed login/register error

--- endpoint/add-user.php
+++ endpoint/add-user.php
@@ -1,55 +1,29 @@
 <?php
-include ('http://localhost/barangay-population-monitoring-system/conn/conn.php');
+// Gamita '../' para mo-back sa parent folder kay naa ka sa 'endpoint' folder
+include('../conn/conn.php');
 
-$firstName = $_POST['first_name'];
-$lastName = $_POST['last_name'];
-$contactNumber = $_POST['contact_number'];
-$email = $_POST['email'];
-$username = $_POST['username'];
-$password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $firstName     = $_POST['first_name'];
+    $lastName      = $_POST['last_name'];
+    $contactNumber = $_POST['contact_number'];
+    $email         = $_POST['email'];
+    $username      = $_POST['username'];
+    $password      = $_POST['password'];
 
+    $stmt = $conn->prepare("INSERT INTO tbl_user (first_name, last_name, contact_number, email, username, password) 
+                            VALUES (:first_name, :last_name, :contact_number, :email, :username, :password)");
 
-try {
-    $stmt = $conn->prepare("SELECT `first_name`, `last_name` FROM `tbl_user` WHERE `first_name` = :first_name AND `last_name` = :last_name");
-    $stmt->execute([
-        'first_name' => $firstName,
-        'last_name'=> $lastName
-    ]);
-    $nameExist = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if (empty($nameExist)) {
-        $conn->beginTransaction();
-
-        $insertStmt = $conn->prepare("INSERT INTO `tbl_user` (`tbl_user_id`, `first_name`, `last_name`, `contact_number`, `email`, `username`, `password`) VALUES (NULL, :first_name, :last_name, :contact_number, :email, :username, :password)");
-        $insertStmt->bindParam(':first_name', $firstName, PDO::PARAM_STR);
-        $insertStmt->bindParam(':last_name', $lastName, PDO::PARAM_STR);
-        $insertStmt->bindParam(':contact_number', $contactNumber, PDO::PARAM_INT);
-        $insertStmt->bindParam(':email', $email, PDO::PARAM_STR);
-        $insertStmt->bindParam(':username', $username, PDO::PARAM_STR);
-        $insertStmt->bindParam(':password', $password, PDO::PARAM_STR);
-        $insertStmt->execute();
-
-
-        echo "
-        <script>
-            alert('Registered Successfully');
-            window.location.href = 'http://localhost/barangay-population-monitoring-system/home.php';
-        </script>
-        ";
-
-        $conn->commit();
-    } else {
-        echo "
-        <script>
-            alert('User Already Exist');
-            window.location.href = 'http://localhost/barangay-population-monitoring-system/home.php';
-        </script>
-        ";
-    }
-
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
+    $stmt->bindParam(':first_name',     $firstName);
+    $stmt->bindParam(':last_name',      $lastName);
+    $stmt->bindParam(':contact_number', $contactNumber);
+    $stmt->bindParam(':email',          $email);
+    $stmt->bindParam(':username',       $username);
+    $stmt->bindParam(':password',       $password);
+    
+    $stmt->execute();
 }
 
-
+// Mo-balik sa registration/login page pag-human
+header("Location: ../index.php");
+exit();
 ?>
